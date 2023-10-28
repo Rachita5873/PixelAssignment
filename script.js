3f8c3ceff09cb4ead6eed541238e3ec1755a1f3c
Rachita5873 <76832946+Rachita5873@users.noreply.github.com>
2023-10-28 15:33:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+  const btn = document.getElementById("btn1");
+
+  btn.addEventListener("click", ()=>{
+    window.location.href = "https://zomi.finance/#/trade";
+  })
+
+
+  const btnn = document.getElementById("btn4");
+  btnn.addEventListener("click", ()=>{
+    window.location.href = "https://zomi.finance/#/trade";
+  })
+
+  const twitter = document.getElementById("twitter");
+
+  twitter.addEventListener("click", () => {
+    window.location.href = "https://twitter.com/zomi.finance";
+  })
+
+  const medium = document.getElementById("medium");
+
+  medium.addEventListener("click", () => {
+    window.location.href = "https://zomifinance.medium.com/";
+  })
+
+  const github = document.getElementById("github");
+
+  github.addEventListener("click", () => {
+    window.location.href = "https://github.com/zomi";
+  })
+
+  const telegram = document.getElementById("telegram");
+
+  telegram.addEventListener("click", () => {
+    window.location.href = "https://t.me/zomichat";
+  })
+
+  const discord = document.getElementById("discord");
+
+  discord.addEventListener("click", () => {
+    window.location.href = "https://discord.com/invite/senshilabs";
+  })
+
+
+  
